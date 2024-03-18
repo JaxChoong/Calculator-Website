f91dfeab3c3ff959486613f68bc626f2c2ce698e
Jax Choong <choongjax@gmail.com>
2024-03-18 12:23:13 +0800

    added Text Display

--- script.js
+++ script.js
@@ -23,16 +23,47 @@ function division( num1, num2 ){
 }
 
 function operate( num1, num2){
-    if (operate === "+"){
+    if ( operate === "+" ){
         return addition( num1, num2 )
     }
-    else if (operate === "-"){
+    else if ( operate === "-" ){
         return subtraction( num1, num2 )
     }
-    else if (operate === "*"){
+    else if ( operate === "*" ){
         return multiplication( num1, num2 )
     }
-    else if (operate === "/"){
+    else if ( operate === "/" ){
         return division( num1, num2 )
     }
+}
+
+const buttons = document.querySelectorAll(".button")
+
+buttons.forEach((button)=>{
+    button.addEventListener("click",()=>{
+        checkButton(button.textContent)})
+})
+
+function checkButton( buttonValue ){
+    if ( buttonValue ==="1" || buttonValue ==="2" || buttonValue ==="3" || buttonValue ==="4" || buttonValue ==="5" || 
+        buttonValue ==="6" || buttonValue ==="7" || buttonValue ==="8" || buttonValue ==="9" || buttonValue ==="0" ){
+        
+        concatString( buttonValue );
+    }
+    else if (buttonValue ==="+" || buttonValue ==="-" || buttonValue ==="*" || buttonValue ==="/" ){
+        concatString( buttonValue );
+    }
+    else if (buttonValue ==="Clear"){
+        clearString()
+    }
+}
+
+const results = document.querySelector("#results")
+function concatString(buttonValue){
+    
+    document.querySelector("#results").textContent = (results.textContent + buttonValue)
+}
+
+function clearString(){
+    document.querySelector("#results").textContent = ""
 }
